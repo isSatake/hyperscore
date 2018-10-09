587013253cab37f150a6f61c325d19d5d583fcbd
stkay <stkayy@gmail.com>
2018-10-09 10:54:50 +0900

    init

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,22 +1,16 @@
-import * as verovio from "verovio-dev";
+import * as abcjs from "abcjs";
 
-// const input = document.getElementById("input");
+const input = document.getElementById("input");
 const div = document.getElementById("svgoutput");
-const vrvToolkit = new verovio.toolkit();
-
-const render = (pae: string): void => {
-    console.log("render", "pae:", pae);
-    div.innerHTML = vrvToolkit.renderData(pae, {
-        inputFormat: "pae",
-        scale: 50,
-        adjustPageHeight: 1,
-        pageWidth: 1048,
-        border: 0,
-        spacingStaff: 2
-    });
+const abcClickListener = (abcElem, tuneNumber, classes) => {
+    console.log("abcClickListener", abcElem, tuneNumber, classes);
 };
 
-render("@clef:G-2@keysig:xFCGD@timesig:3/8@data:'6B/{8B+(6B''E'B})({AFD})/{6.E3G},8B-/({6'EGF})({FAG})({GEB})/4F6-");
-// input.addEventListener("input", e => {
-//     render((e.target as HTMLInputElement).value);
-// });
+input.addEventListener("input", e => {
+    const ABC: string = (e.target as HTMLInputElement).value;
+    const options = {
+        clickListener: abcClickListener,
+        add_classes: true
+    };
+    abcjs.renderAbc("svgoutput", ABC, options);
+});
