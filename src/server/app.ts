04ccba9ef725336fb651317ca3c96af94f2d7bdd
stkay <stkayy@gmail.com>
2018-10-11 11:41:38 +0900

    port

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,7 +4,7 @@ const app: express.Express = express();
 
 app.use(express.static('public'));
 
-const port = "3000";
+const port = process.env.PORT || "3000";
 app.set('port', port);
 
 const server = http.createServer(app);
